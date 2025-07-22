014025b8300aa2aa4bb9424f0155d406dfaccf39
Ujjawal Shrivastava <144740929+shrujj01@users.noreply.github.com>
2025-07-22 10:51:51 -0400

    Time: 0 ms (100.00%) | Memory: 45.2 MB (71.18%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -23,13 +23,4 @@ class Solution {
         int sum = (n*(n+1))/2;
         return sum;
     }
-     public int findMaxNumber(int[] nums){
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>=max){
-                max=nums[i];
-            }
-        }
-        return max;
-     }
 }
