7d8a5b5fd305d711329b697a16817d956a9e6872
Ujjawal Shrivastava <144740929+shrujj01@users.noreply.github.com>
2025-08-09 04:04:45 -0400

    Time: 28 ms (81.47%) | Memory: 52.6 MB (19.16%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,38 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> finalList = new ArrayList<>();
+        List<List<Integer>> finalList = new ArrayList<List<Integer>>();
         Arrays.sort(nums);
-        for(int i =0;i<nums.length;i++){
-            if(i>0 && nums[i]==nums[i-1]){
+
+        for(int i = 0;i<nums.length;i++){
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
-            int start = i+1;
-            int end = nums.length-1;
-            
+            int start = i+1, end = nums.length-1;
             while(start<end){
-                int target = nums[i]+nums[start]+nums[end];
-                if(target == 0){
-                    ArrayList<Integer> list = new ArrayList<Integer>();
-                    list.add(nums[i]);
-                    list.add(nums[start]);
-                    list.add(nums[end]);
-                    finalList.add(list);
+                
+                int sum = nums[i] + nums[start] + nums[end];
+                if(sum == 0){
+                    List<Integer> triplet = new ArrayList<Integer>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[start]);
+                    triplet.add(nums[end]);
+
+                    finalList.add(triplet);
                     start++;
 
                     while(nums[start] == nums[start-1] && start<end){
-                        start++;
+                    start++;
                     }
                 }
-                else if(target>0){
-                    end--;
+                if(sum<0){
+                    start++;
                 }
                 else{
-                    start++;
+                    end--;
                 }
             }
         }
+
         return finalList;
     }
 }
