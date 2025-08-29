a4277ee37dbc135f76b9b9b2cd1c2fbde416eba7
Ujjawal Shrivastava <144740929+shrujj01@users.noreply.github.com>
2025-08-30 01:28:17 +0530

    Time: 13 ms (20.67%) | Memory: 45 MB (16.74%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[i].length;j++){
+
+                char number = board[i][j];
+                if(number != '.'){
+                        if(
+                            !seen.add(number + "found in row" + i) ||
+                            !seen.add(number + "found in column" + j) ||
+                            !seen.add(number + "found in subbox" + i/3 + "-" + j/3)    
+                        )
+                        {
+                            return false;
+                        }
+                }
+            }
+        }
+        return true;
+    }
+}
