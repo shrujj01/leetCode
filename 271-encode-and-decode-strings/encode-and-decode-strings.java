692ddd2054604d789bdedb553658c7510694f1c2
Ujjawal Shrivastava <144740929+shrujj01@users.noreply.github.com>
2025-08-31 03:46:31 +0530

    Time: 16 ms (20.48%) | Memory: 45.9 MB (23.20%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -4,7 +4,7 @@ public class Codec {
     public String encode(List<String> strs) {
         StringBuffer out = new StringBuffer();
         for(String s : strs){
-            out.append(s.replace("#","##")).append(" # ");
+            out.append(s.replace("#","##")).append(" & ");
         }
         return out.toString();
     }
@@ -12,7 +12,7 @@ public class Codec {
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List strs = new ArrayList<>();
-        String[] array = s.split(" # ", -1);
+        String[] array = s.split(" & ", -1);
         for(int i = 0 ; i<array.length-1;i++){
             strs.add(array[i].replace("##","#"));
         }
